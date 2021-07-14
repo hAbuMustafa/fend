69770c6f0cfe211e74c661d86c7aa294e4f1224d
Hosam Sultan <hosam5553@gmail.com>
2021-07-14 16:32:43 +0200

    Message (Don for the most part AND the first suggested task)

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,26 +17,56 @@
  * Define Global Variables
  * 
 */
-
-
+const sectionTitles = Array.from(document.querySelectorAll('.section__title'));
+const navBarList = document.querySelector('#navbar__list');
+var menuList;
+const intersectionObserver = new IntersectionObserver(intersectionObserverCallback, {threshold: 0.5});
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
-
+ */
+function intersectionObserverCallback(entry){
+    const target = entry[0].target;
+    if (entry[0].intersectionRatio>=0.5){
+        target.classList.add('your-active-class');
+        for (navItem of menuList){
+            if(navItem.firstChild.innerText === target.getAttribute('data-nav')){
+                navItem.classList.add('active');
+            } else {
+                navItem.classList.remove('active');
+            }
+        }
+    } else {
+        target.classList.remove('your-active-class');
+    }
+}
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
+function populateNavBar(){
+    sectionTitles.forEach((sectionTitle)=> {
+        let listItem = `<a href="#${sectionTitle.id}" class='menu__link'><li>${sectionTitle.getAttribute('data-nav')}</li></a>`;
+        navBarList.innerHTML+=listItem;
+    })
+    menuList = Array.from(navBarList.querySelectorAll('.menu__link'));
+}
+
+
 
 
 // Add class 'active' to section when near top of viewport
+function addSectionTitleObserver(){
+    for (sectionTitle of sectionTitles){
+        intersectionObserver.observe(sectionTitle);
+    }
+}
 
 
 // Scroll to anchor ID using scrollTO event
@@ -49,9 +79,12 @@
 */
 
 // Build menu 
+document.addEventListener('DOMContentLoaded', ()=>{
+    populateNavBar();
+})
 
 // Scroll to section on link click
+// DONE! Through CSS option scroll-behavior
 
 // Set sections as active
-
-
+addSectionTitleObserver();
